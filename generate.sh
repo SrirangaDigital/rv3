bc24e019c25fa6fc5f91322268bf7f152ed91af0
Arjuna <arjun.kashyap@yahoo.co.in>
2014-07-09 22:23:20 +0530

    XML corrections

--- generate.sh
+++ generate.sh
@@ -9,5 +9,5 @@ echo "drop database rv; create database rv charset utf8 collate utf8_general_ci;
 
 perl insert_author.pl $host $db $usr $pwd
 perl insert_articles.pl $host $db $usr $pwd
-perl ocr.pl $host $db $usr $pwd
-perl searchtable.pl $host $db $usr $pwd
+#~ perl ocr.pl $host $db $usr $pwd
+#~ perl searchtable.pl $host $db $usr $pwd
